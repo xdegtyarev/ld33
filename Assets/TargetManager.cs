2547394b1ea395979a3f6341661703c62334c09b
Alexander Degytarev <xdegtyarev@gmail.com>
2015-08-24 00:59:10 +0300

    fixed bug with non hit cards

--- Assets/TargetManager.cs
+++ Assets/TargetManager.cs
@@ -5,14 +5,14 @@ public class TargetManager : MonoBehaviour {
 	public static IHitReceiver GetTarget(CardView view){
 		if(Player.instance.playerPlayfield.GetCardViewRow(view) != -1){
 			var playerTarget = Enemy.instance.playerPlayfield.GetCardView(Player.instance.playerPlayfield.GetCardViewRow(view));
-			if(playerTarget!=null){
+			if(playerTarget!=null && playerTarget.GetComponent<IHitReceiver>()!=null){
 				return playerTarget.GetComponent<IHitReceiver>();
 			}else{
 				return Enemy.instance;
 			}
 		}else{
 			var enemyTarget = Player.instance.playerPlayfield.GetCardView(Enemy.instance.playerPlayfield.GetCardViewRow(view));
-			if(enemyTarget!=null){
+			if(enemyTarget!=null && enemyTarget.GetComponent<IHitReceiver>()!=null){
 				return enemyTarget.GetComponent<IHitReceiver>();
 			}else{
 				return Player.instance;
